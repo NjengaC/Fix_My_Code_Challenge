cc98302de8b49fea1384b9dd96445d1505dfeaeb
NjengaC <victorcyrus01@gmail.com>
2024-03-16 15:20:11 +0300

    send

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """
 File Was not Documented,
-Gets defines class square, area and perometer methods and also modifies __str__
+Gets defines class square, area and perimeter methods and also modifies __str__
 """
 
 
-class Square():
+class Square:
     """
     Class square defines a square object
     """
-    height = 0
-    width = 0
 
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        if self.width == self.height:
-            return self.width * self.height
+        """ Calculate the area of the square """
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
-        """ Calculate the perimeter of square """
-        if self.width == self.height:
-            return (self.width * 2) + (self.height * 2)
+        """ Calculate the perimeter of the square """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         """ String representation of the square """
@@ -34,5 +30,5 @@ class Square():
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
