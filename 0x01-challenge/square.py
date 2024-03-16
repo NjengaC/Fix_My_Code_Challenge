b7751c4235840eecc64bcfb984d5db720a9908ff
NjengaC <victorcyrus01@gmail.com>
2024-03-16 14:50:54 +0300

    height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,7 +18,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """ Calculate the perimeter of the square """
